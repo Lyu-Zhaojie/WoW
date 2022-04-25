a16b43081652992604e88587c7454b4a94765337
Lyu-Zhaojie <lyuzhaojie@outlook.com>
2022-04-25 10:56:55 +0800

    WoW4!

--- WoW4.cpp
+++ WoW4.cpp
@@ -1,4 +1,5 @@
 #include <iomanip>
+#include <ios>
 #include <iostream>
 #include <vector>
 #include <wchar.h>
@@ -44,20 +45,24 @@ protected:
 
     public:
         WeaponList(int weapon1, int weapon2, int warriorForce);
-        bool hasSword() const { return mSword > 0; }
         int getSwordForce() const { return mSword; }
+        int getArrowNum() const { return mArrow; }
+        int getBombNum() const { return mBomb; }
+        bool hasSword() const { return mSword > 0; }
         bool hasBomb() const { return mBomb > 0; }
         bool hasArrow() const { return mArrow > 0; }
         void useSword() { mSword = mSword * 8 / 10; }
         void useArrow() { mArrow -= 1; }
-        void useBomb() { mBomb -= 1; }
+        bool hasWeapon() const { return hasSword() || hasBomb() || hasArrow(); }
     } weaponList;
 
 public:
     BasicWarrior(int id, Command *head, Type type, int element, int force, int weapon1 = -1, int weapon2 = -1);
     virtual ~BasicWarrior() {}
 
-    int getDamage() { return mForce + weaponList.getSwordForce(); }
+    int getDamage() const { return mForce + weaponList.getSwordForce(); }
+    int getBackDamage() const { return mForce / 2 + weaponList.getSwordForce(); }
+
     virtual void yell() {} // For Dragon
 
     void useBomb(BasicWarrior *enemy);
@@ -68,6 +73,7 @@ public:
 
     void sendElement(int element);
     virtual void beforeMove() {}
+    void reportWeapon();
 
     bool isAlive() const { return mElement > 0; }
     virtual bool isLoyal() const { return true; }
@@ -82,14 +88,18 @@ private:
 
 public:
     Dragon(int id, Command *head, double morale)
-        : BasicWarrior(id, head, DRAGON, INIT_ELEMENT[DRAGON], INIT_FORCE[DRAGON], id % 3), mMorale{morale} {}
+        : BasicWarrior(id, head, DRAGON, INIT_ELEMENT[DRAGON], INIT_FORCE[DRAGON], id % 3),
+          mMorale{morale}
+    {
+        cout << "Its morale " << setiosflags(ios::fixed) << setprecision(2) << mMorale << endl;
+    }
     void yell() override;
 };
 class Ninja : public BasicWarrior
 {
 public:
     Ninja(int id, Command *head)
-        : BasicWarrior(id, head, DRAGON, INIT_ELEMENT[DRAGON], INIT_FORCE[DRAGON], id % 3, (id + 1) % 3) {}
+        : BasicWarrior(id, head, NINJA, INIT_ELEMENT[NINJA], INIT_FORCE[NINJA], id % 3, (id + 1) % 3) {}
     void foughtBack(BasicWarrior *enemy) override {}
 };
 class Iceman : public BasicWarrior
@@ -99,7 +109,7 @@ private:
 
 public:
     Iceman(int id, Command *head)
-        : BasicWarrior(id, head, DRAGON, INIT_ELEMENT[DRAGON], INIT_FORCE[DRAGON], id % 3),
+        : BasicWarrior(id, head, ICEMAN, INIT_ELEMENT[ICEMAN], INIT_FORCE[ICEMAN], id % 3),
           moveTimes{0} {}
     void beforeMove() override;
 };
@@ -109,11 +119,19 @@ private:
     int mLoyalty;
 
 public:
+    Lion(int id, Command *head, int loyalty)
+        : BasicWarrior(id, head, LION, INIT_ELEMENT[LION], INIT_FORCE[LION]),
+          mLoyalty{loyalty}
+    {
+        cout << "Its loyalty is " << mLoyalty << endl;
+    }
     bool isLoyal() const override { return mLoyalty > 0; }
 };
 class Wolf : public BasicWarrior
 {
 public:
+    Wolf(int id, Command *head)
+        : BasicWarrior(id, head, WOLF, INIT_ELEMENT[WOLF], INIT_FORCE[WOLF]) {}
 };
 class City
 {
@@ -131,10 +149,21 @@ public:
         : mID{id},
           mWarriorPtr{nullptr, nullptr}, tmpWarriorPtr{nullptr, nullptr},
           mWestCity{westCity}, mEastCity{eastCity}, mElement{element}, mFlag{flag} {}
+    ~City()
+    {
+        for (Flag i : {RED, BLUE})
+        {
+            if (mWarriorPtr[i])
+                delete mWarriorPtr[i];
+            if (tmpWarriorPtr[i])
+                delete tmpWarriorPtr[i];
+        }
+    }
     void letWarriorGetElement();
     virtual void lionEscape();
     void generateElement() { mElement += 10; }
     int getID() const { return mID; }
+    BasicWarrior *getWarriorPtr(Flag flag) { return mWarriorPtr[flag]; }
 };
 class Command : public City
 {
@@ -147,9 +176,11 @@ public:
     Command(Flag flag, int element, City *westCity, City *eastCity)
         : City(-1, westCity, eastCity, element, flag),
           generateIndex{0}, nOfWarrior{0} {}
+    void lionEscape() override;
     void generate();
     Flag getHead() const { return mFlag; }
     void addElement(int element) { mElement += element; }
+    void reportElement();
 };
 
 int main()
@@ -194,17 +225,37 @@ BasicWarrior::BasicWarrior(int id, Command *head, Type type, int element, int fo
     : weaponList(weapon1, weapon2, mForce),
       mID{id},
       mCommand{head}, mFlag{mCommand->getHead()},
-      mType{type}, mElement{element}, mForce{force} {}
+      mType{type}, mElement{element}, mForce{force}
+{
+    showTime();
+    cout << HEAD_NAME[mFlag] << ' ' << WARRIOR_NAME[mType] << ' ' << mID << " born" << endl;
+}
 
 void BasicWarrior::useBomb(BasicWarrior *enemy)
 {
-    if (enemy->getDamage() >= mElement)
+    bool judge; // TODO: judge whether use bomb or not.
+    if (judge)
     {
         beAttacked(mElement);
         enemy->beAttacked(enemy->mElement);
+        showTime();
+        cout << HEAD_NAME[mFlag] << ' ' << WARRIOR_NAME[mType] << ' ' << mID << " used a bomb and killed " << HEAD_NAME[enemy->mFlag] << ' ' << WARRIOR_NAME[enemy->mType] << ' ' << enemy->mID << endl;
     }
 }
-void BasicWarrior::useArrow(City *nextCity) {}
+void BasicWarrior::useArrow(City *nextCity)
+{
+    if (nextCity && weaponList.hasArrow())
+    {
+        BasicWarrior *enemy{nextCity->getWarriorPtr(mFlag ^ 1)};
+        enemy->beAttacked(ARROW_FORCE);
+        weaponList.useArrow();
+        showTime();
+        if (enemy->isAlive())
+            cout << HEAD_NAME[mFlag] << ' ' << WARRIOR_NAME[mType] << ' ' << mID << " shot" << endl;
+        else
+            cout << HEAD_NAME[mFlag] << ' ' << WARRIOR_NAME[mType] << ' ' << mID << " shot and killed " << HEAD_NAME[enemy->mFlag] << ' ' << WARRIOR_NAME[enemy->mType] << ' ' << enemy->mID << endl;
+    }
+}
 void BasicWarrior::attack(BasicWarrior *enemy, int cityID)
 {
     showTime();
@@ -217,9 +268,29 @@ void BasicWarrior::attack(BasicWarrior *enemy, int cityID)
     if (weaponList.hasSword())
         weaponList.useSword();
 }
-void BasicWarrior::foughtBack(BasicWarrior *enemy) {}
+void BasicWarrior::foughtBack(BasicWarrior *enemy)
+{
+    if (isAlive())
+        enemy->beAttacked(getBackDamage());
+}
 void BasicWarrior::sendElement(int element) { mCommand->addElement(element); }
-
+void BasicWarrior::reportWeapon()
+{
+    showTime();
+    cout << HEAD_NAME[mFlag] << ' ' << WARRIOR_NAME[mType] << ' ' << mID << " has ";
+    if (!weaponList.hasWeapon())
+        cout << "no weapon" << endl;
+    else
+    {
+        if (weaponList.hasArrow())
+            cout << "arrow(" << weaponList.getArrowNum() << ')';
+        if (weaponList.hasBomb())
+            cout << "bomb";
+        if (weaponList.hasSword())
+            cout << "sword(" << weaponList.getSwordForce() << ')';
+        cout << endl;
+    }
+}
 void Dragon::yell() {}
 
 void Iceman::beforeMove()
@@ -239,7 +310,7 @@ void Iceman::beforeMove()
 
 void City::lionEscape()
 {
-    for (int i : {RED, BLUE})
+    for (Flag i : {RED, BLUE})
     {
         if (mWarriorPtr[i] && mWarriorPtr[i]->getType() == LION && !mWarriorPtr[i]->isLoyal())
         {
@@ -264,6 +335,16 @@ void City::letWarriorGetElement()
     }
 }
 
+void Command::lionEscape()
+{
+    if (mWarriorPtr[mFlag] && mWarriorPtr[mFlag]->getType() == LION && !mWarriorPtr[mFlag]->isLoyal())
+    {
+        showTime();
+        cout << HEAD_NAME[mFlag] << " lion " << mWarriorPtr[mFlag]->getID() << " ran away" << endl;
+        delete mWarriorPtr[mFlag];
+        mWarriorPtr[mFlag] = nullptr;
+    }
+}
 void Command::generate()
 {
     int generateType{ORDER[mFlag][generateIndex]};
@@ -280,10 +361,20 @@ void Command::generate()
             mWarriorPtr[mFlag] = new Ninja(nOfWarrior, this);
             break;
         case ICEMAN:
+            mWarriorPtr[mFlag] = new Iceman(nOfWarrior, this);
+            break;
         case LION:
+            mWarriorPtr[mFlag] = new Lion(nOfWarrior, this, mElement);
+            break;
         case WOLF:
+            mWarriorPtr[mFlag] = new Wolf(nOfWarrior, this);
             break;
         }
         generateIndex = generateType == 4 ? 0 : generateType + 1;
     }
+}
+void Command::reportElement()
+{
+    showTime();
+    cout << mElement << " in " << HEAD_NAME[mFlag] << " headquarter" << endl;
 }
